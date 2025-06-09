463a5a1e937db1168515658077524918da60adc7
Daniel Ikechukwu <ikechukwudaniel080@gmail.com>
2025-06-09 11:47:15 +0100

    feat(context): Implement LogEnteries in DbContext.

---
+++ CustomLoggerImplementation/Models/LogEntry.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomLoggerImplementation.Models
+{
+    public class LogEntry
+    {
+        public int Id { get; set; }
+
+        // The logger category name
+        public string? Category { get; set; }
+
+        // The actual log message 
+        public string? Message { get; set; }
+
+        // The log level (e.g. Information, Error, etc.)
+        public string LogLevel { get; set; }
+
+        //Optional EventId
+        public int? EventId { get; set; }
+
+        // Exception details if any
+        public string? Exception { get; set; }
+
+        // The time when the log was created
+        public DateTime CreatedTime { get; set; }
+
+    }
+}
---
+++ CustomLoggerImplementation/Models/LoggingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CustomLoggerImplementation.Models
+{
+    public class LoggingDbContext : DbContext
+    {
+        public LoggingDbContext(DbContextOptions<LoggingDbContext> options) : base(options)
+        {
+        }
+
+        // A DbSet to hold our log entries
+        public DbSet<LogEntry> LogEntries { get; set; }
+    }
+}
